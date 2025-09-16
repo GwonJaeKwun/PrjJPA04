b4498d4c4f7d4096f13f02a6560c8af70cb6ddc5
GwonJaeKwun <nannada1590@gmail.com>
2025-09-16 17:51:29 +0900

    PrjJpa04 댓글작업

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# SpringJpa3
-# PrjJPA04
+# SpringJpa4
--- src/main/java/com/green/dto/Comments.java
+++ src/main/java/com/green/dto/Comments.java
@@ -13,35 +13,38 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Data
-@NoArgsConstructor  //기본생성자
-@AllArgsConstructor
+@Data     // @Getter + @Setter + @RToStirng + eqauls + hascode + ..
+@NoArgsConstructor   // 기본생성자 
+@AllArgsConstructor  // 모든인자 있는 생성자
 @SequenceGenerator(
-		name="COMMENT_SEQ_GENERATOR",
-		sequenceName="COMMENTS_SEQ",  //create sequence COMMENT_SEQ
-		initialValue=1,     //초기값  start with 1
-		allocationSize=1    //증가치  increment by1)
-		)
-public class Comments {
-		
-	@Id                         //primary key
+	name = "COMMENTS_SEQ_GENERATOR",
+	sequenceName   = "COMMENTS_SEQ",  // create sequence COMMENT_SEQ
+	initialValue   = 1,     // 초기값   start    with 1
+	allocationSize = 1      // 증가치   increment by 1)
+)
+public class Comments {   // comment : ORA-00903: 테이블명이 부적합합니다
+	
+	@Id                          // primary key
 	@GeneratedValue(strategy = GenerationType.SEQUENCE,
-	         generator = "COMMENT_SEQ_GENERATOR")           //번호자동증가 sequence
-	private Long id;
+			generator = "COMMENTS_SEQ_GENERATOR")   // 번호자동증가 sequence
+	private   Long     id;
 	
-	// @Column(name="body", nullable=true, length=255)
-	// Oracle 11g varchar2 최대4000, CLOB
-	// Oracle 12c varchar2 최대 32000 -> 별도설정 필요
+	// @Column(name="nick_name", nullable=true, length=255)
+	// Oracle 11g varchar2 최대 4000, -> CLOB
+	// Oracle 12c varchar2 최대 32000  -> 별도설정 필요,   
 	@Column
-	private String body;
+	private   String   body;
 	
 	@Column
-	private String nickname;
-
+	private   String   nickname;
 	
-	//외래키 설정
-	@ManyToOne                     // 외래키 다대일관계
-	@JoinColumn(name="article_id") // 외래키 칼럼
-	private Article article;       // 연결될 entity 객체의 이름
+	// 외래키 설정	
+	@ManyToOne                      // 외래키 다대일관걔
+	@JoinColumn(name="article_id")  // 외래키 칼럼
+	private   Article   article;    // 연결될 entity 객체의 이름 
 	
 }
+
+
+
+
